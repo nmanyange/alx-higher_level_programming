73d7eb39e66b85bea9c52aae7929561cd7f08fcd
Nyaradzo <nmanyange30@gmail.com>
2023-12-19 03:23:32 -0800

    classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Square module"""
+
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size):
+        """Initialize a square.
+
+        Args:
+          size: length of side of the square.
+        """
+        self.__size = size
