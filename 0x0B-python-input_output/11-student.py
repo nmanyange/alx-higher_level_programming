8ebd14e24566cbc729a62ee811314ee0bbfe919a
Nyaradzo <nmanyange30@gmail.com>
2024-01-09 04:41:48 -0800

    task 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -25,4 +25,12 @@ class Student:
         if attrs is None:
             return self.__dict__
         else:
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the student
+        Args:
+            json(dict): The key/value to replace the attributes with
+        """
+        for k, v in json.items():
+            setattr(self, k, v)
